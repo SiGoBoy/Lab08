ab33c4efeed629ff9f889d3b50c4b228c4a1f65b
SiGoBoy <kuzmenkofilaret1@gmail.com>
2025-09-17 21:42:33 +0300

    made lots of changes and it worked

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
-
 #include "formatter_ex.h"
 #include "solver.h"
 
-int main()
-{
-    float a = 0;
-    float b = 0;
-    float c = 0;
-
+int main() {
+    float a, b, c;
+    std::cout << "Enter coefficients a, b, c: ";
     std::cin >> a >> b >> c;
 
-    float x1 = 0;
-    float x2 = 0;
+    float x1, x2;
+    solve(a, b, c, x1, x2);
 
-    try
-    {
-        solve(a, b, c, x1, x2);
-
-        formatter(std::cout, "x1 = " + std::to_string(x1));
-        formatter(std::cout, "x2 = " + std::to_string(x2));
-    }
-    catch (const std::logic_error& ex)
-    {
-        formatter(std::cout, ex.what());
-    }
+    std::cout << "Roots: x1 = " << x1 << ", x2 = " << x2 << std::endl;
 
     return 0;
 }
