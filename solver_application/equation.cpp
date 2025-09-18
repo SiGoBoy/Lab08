b5c888b3b5af981ac2affd7075e5e18db97cbdc8
SiGoBoy <kuzmenkofilaret1@gmail.com>
2025-09-18 14:17:39 +0300

    changed solver_application

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
+
 #include "formatter_ex.h"
 #include "solver.h"
 
-int main() {
-    float a, b, c;
-    std::cout << "Enter coefficients a, b, c: ";
+int main()
+{
+    float a = 0;
+    float b = 0;
+    float c = 0;
+
     std::cin >> a >> b >> c;
 
-    float x1, x2;
-    solve(a, b, c, x1, x2);
+    float x1 = 0;
+    float x2 = 0;
 
-    std::cout << "Roots: x1 = " << x1 << ", x2 = " << x2 << std::endl;
+    try
+    {
+        solve(a, b, c, x1, x2);
+
+        formatter(std::cout, "x1 = " + std::to_string(x1));
+        formatter(std::cout, "x2 = " + std::to_string(x2));
+    }
+    catch (const std::logic_error& ex)
+    {
+        formatter(std::cout, ex.what());
+    }
 
     return 0;
 }
